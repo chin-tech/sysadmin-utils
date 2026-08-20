716c68802c4910825484509ec353eee8ddcb8ce0
Alexander <alex@chin-tech.org>
2026-08-19 21:18:40 -0700

    sha

---
+++ password_party/Mobiles/Sha512.ps1
@@ -0,0 +1,131 @@
+$Sha512CryptSource = @"
+using System;
+using System.Text;
+using System.Security.Cryptography;
+
+public class Sha512Crypt
+{
+    private const string B64Alphabet = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public static string Crypt(string password, string salt = null, int rounds = 5000)
+    {
+        if (salt == null)
+        {
+            byte[] saltBytes = new byte[12];
+            using (var rng = RandomNumberGenerator.Create()) rng.GetBytes(saltBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 12; i++) sb.Append(B64Alphabet[saltBytes[i] % 64]);
+            salt = sb.ToString();
+        }
+        else if (salt.Length > 16)
+        {
+            salt = salt.Substring(0, 16);
+        }
+
+        byte[] keyBytes = Encoding.UTF8.GetBytes(password);
+        byte[] saltBytesArray = Encoding.UTF8.GetBytes(salt);
+
+        using (SHA512 sha = SHA512.Create())
+        {
+            // Digest B
+            sha.TransformBlock(keyBytes, 0, keyBytes.Length, null, 0);
+            sha.TransformBlock(saltBytesArray, 0, saltBytesArray.Length, null, 0);
+            sha.TransformFinalBlock(keyBytes, 0, keyBytes.Length);
+            byte[] altResult = sha.Hash;
+
+            // Digest A
+            sha.Initialize();
+            sha.TransformBlock(keyBytes, 0, keyBytes.Length, null, 0);
+            sha.TransformBlock(saltBytesArray, 0, saltBytesArray.Length, null, 0);
+            
+            for (int i = keyBytes.Length; i > 64; i -= 64)
+                sha.TransformBlock(altResult, 0, 64, null, 0);
+            if (keyBytes.Length % 64 > 0)
+                sha.TransformBlock(altResult, 0, keyBytes.Length % 64, null, 0);
+
+            for (int i = keyBytes.Length; i > 0; i >>= 1)
+            {
+                if ((i & 1) != 0)
+                    sha.TransformBlock(altResult, 0, 64, null, 0);
+                else
+                    sha.TransformBlock(keyBytes, 0, keyBytes.Length, null, 0);
+            }
+            sha.TransformFinalBlock(new byte[0], 0, 0);
+            byte[] pResult = sha.Hash;
+
+            // P-Sequence
+            sha.Initialize();
+            for (int i = 0; i < keyBytes.Length; i++)
+                sha.TransformBlock(keyBytes, 0, keyBytes.Length, null, 0);
+            sha.TransformFinalBlock(new byte[0], 0, 0);
+            byte[] pBytes = new byte[keyBytes.Length];
+            for (int i = 0; i < keyBytes.Length; i++)
+                pBytes[i] = sha.Hash[i % 64];
+
+            // S-Sequence
+            sha.Initialize();
+            for (int i = 0; i < 16 + pResult[0]; i++)
+                sha.TransformBlock(saltBytesArray, 0, saltBytesArray.Length, null, 0);
+            sha.TransformFinalBlock(new byte[0], 0, 0);
+            byte[] sBytes = new byte[saltBytesArray.Length];
+            for (int i = 0; i < saltBytesArray.Length; i++)
+                sBytes[i] = sha.Hash[i % 64];
+
+            // 5000+ Rounds
+            for (int i = 0; i < rounds; i++)
+            {
+                sha.Initialize();
+                if ((i & 1) != 0)
+                    sha.TransformBlock(pBytes, 0, pBytes.Length, null, 0);
+                else
+                    sha.TransformBlock(pResult, 0, 64, null, 0);
+
+                if (i % 3 != 0)
+                    sha.TransformBlock(sBytes, 0, sBytes.Length, null, 0);
+
+                if (i % 7 != 0)
+                    sha.TransformBlock(pBytes, 0, pBytes.Length, null, 0);
+
+                if ((i & 1) != 0)
+                    sha.TransformBlock(pResult, 0, 64, null, 0);
+                else
+                    sha.TransformBlock(pBytes, 0, pBytes.Length, null, 0);
+
+                sha.TransformFinalBlock(new byte[0], 0, 0);
+                pResult = sha.Hash;
+            }
+
+            // Custom Base64 Encoding
+            StringBuilder res = new StringBuilder();
+            if (rounds != 5000) res.AppendFormat("`$6`$rounds={0}`${1}`$", rounds, salt);
+            else res.AppendFormat("`$6`${0}`$", salt);
+
+            int[][] order = new int[][] {
+                new int[] {0, 21, 42}, new int[] {22, 43, 1}, new int[] {44, 2, 23},
+                new int[] {3, 24, 45}, new int[] {25, 46, 4}, new int[] {47, 5, 26},
+                new int[] {6, 27, 48}, new int[] {28, 49, 7}, new int[] {50, 8, 29},
+                new int[] {9, 30, 51}, new int[] {31, 52, 10}, new int[] {53, 11, 32},
+                new int[] {12, 33, 54}, new int[] {34, 55, 13}, new int[] {56, 14, 35},
+                new int[] {15, 36, 57}, new int[] {37, 58, 16}, new int[] {59, 17, 38},
+                new int[] {18, 39, 60}, new int[] {40, 61, 19}, new int[] {62, 20, 41}
+            };
+
+            foreach (var trio in order)
+            {
+                int val = (pResult[trio[0]] << 16) | (pResult[trio[1]] << 8) | pResult[trio[2]];
+                for (int j = 0; j < 4; j++) { res.Append(B64Alphabet[val & 0x3F]); val >>= 6; }
+            }
+
+            int lastVal = pResult[63];
+            for (int j = 0; j < 2; j++) { res.Append(B64Alphabet[lastVal & 0x3F]); lastVal >>= 6; }
+
+            return res.ToString();
+        }
+    }
+}
+"@
+
+# Load the class into memory once per session
+if (-not ([System.Management.Automation.PSTypeName]'Sha512Crypt').Type) {
+    Add-Type -TypeDefinition $Sha512CryptSource
+}
