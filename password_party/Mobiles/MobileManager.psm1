969cf3b3e0e0c9497dff8a1541cff08cdbacbe85
Alexander <alex@chin-tech.org>
2026-09-20 21:09:23 -0700

    logging off windows users before removing

--- password_party/Mobiles/MobileManager.psm1
+++ password_party/Mobiles/MobileManager.psm1
@@ -3095,8 +3095,8 @@ function Format-DeploymentResults {
                 '-'
             } elseif ($accountActions.Status -contains 'Failed') { 'FAILED' } else { 'OK' }
 
-            Write-Host $userDefs
-            $password = if ($userDefs.MustChangePassword) { 'DefaultPasswordSet' } else { 'UserSet' }
+            Write-Host "$baseName - $($userDefs.MustChangePassword)"
+            $password = if ($true -in $userDefs.MustChangePassword ) { 'DefaultPasswordSet' } else { 'UserSet' }
             if ($unRegister) { $password = ""}
 
             [PSCustomObject]@{
